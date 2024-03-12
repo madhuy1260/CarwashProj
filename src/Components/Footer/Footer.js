27ce4ff6c6b6032ea48d10bcefbd891d3b58eb88
madhuy1260 <y.madhu1260@gmail.com>
2024-03-12 21:27:21 +0530

    social icons added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,12 +6,13 @@ import {
   faVoicemail,
   faAddressCard,
 } from "@fortawesome/free-solid-svg-icons";
+import { SocialIcon } from "react-social-icons";
 
 function Footer() {
   return (
     <div className="container-fluid footer">
       <div className="row">
-        <div className="col-sm-3 footerDetails">
+        <div className="col-sm-4 footerDetails">
           <p>
             <FontAwesomeIcon icon={faPhone} style={{ paddingLeft: "1rem" }} />{" "}
             Phone :{" "}
@@ -29,7 +30,7 @@ function Footer() {
           </p>
           <p>Yarishh@gmail.com</p>
         </div>
-        <div className="col-sm-3 footerDetails">
+        <div className="col-sm-4 footerDetails">
           <p>
             {" "}
             <FontAwesomeIcon
@@ -44,12 +45,15 @@ function Footer() {
       <div className="row" style={{ margin: "2rem", fontSize: "2rem" }}>
         Logo
       </div>
-      <div className="row" style={{ marginBottom: "10rem" }}>
+      <div className="row">
         <div className="col-sm-3">For Company</div>
         <div className="col-sm-3">For Customers</div>
         <div className="col-sm-3">For Partners</div>
         <div className="col-sm-3">
-          <FontAwesomeIcon icon={faSearch} style={{ paddingLeft: "1rem" }} />
+          <SocialIcon url="https://twitter.com" />{" "}
+          <SocialIcon url="https://facebook.com" />{" "}
+          <SocialIcon url="https://instagram.com" />{" "}
+          <SocialIcon url="https://linkedin.com" />{" "}
         </div>
       </div>
     </div>
