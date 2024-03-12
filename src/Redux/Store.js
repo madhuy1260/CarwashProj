de11bf3853c0bcd92d31a7a123fdcea9ac75e400
madhuy1260 <y.madhu1260@gmail.com>
2024-03-12 18:12:26 +0530

    middlewear removed

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,15 +2,15 @@ import { createLogger } from "redux-logger";
 import { thunk } from "redux-thunk";
 import { applyMiddleware, createStore } from "redux";
 import { commonReducer } from "./CommonReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import INITIAL_STATE from "../public/INITIAL_STATE";
 
 const logger = createLogger();
 
 const store = createStore(
   commonReducer,
-  INITIAL_STATE,
-  composeWithDevTools(applyMiddleware(thunk, logger))
+  INITIAL_STATE
+  // composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 export default store;
